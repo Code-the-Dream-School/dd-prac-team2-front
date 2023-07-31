3a25de9d69f745bdfdc27f8425f1eef8397382e9
Saul Castillo <secch97@gmail.com>
2023-07-31 00:58:28 -0600

    1. Moved Styled components out of component definition

--- src/components/Button/AppButton.jsx
+++ src/components/Button/AppButton.jsx
@@ -12,24 +12,19 @@ import PropTypes from 'prop-types';
 */
 import React from 'react';
 
-/*
-    ==========================
-    =         STYLES         =
-    ==========================
-*/
-const StyledButton = styled(Button)(() => ({
-    backgroundColor: "#C84B31",
-    width: width,
-    transform: "scale(1.0)",
-    transition: "all 0.2s ease-in-out",
-    "&:hover": {
-        backgroundColor: "#C84B31",
-        transform: "scale(1.05)",
-        transition: "all 0.2s ease-in-out"
-    },
-}));
-
 const AppButton = ({children, text, type, width, handlerFunction}) => {
+    const StyledButton = styled(Button)(() => ({
+        backgroundColor: "#C84B31",
+        width: width,
+        transform: "scale(1.0)",
+        transition: "all 0.2s ease-in-out",
+        "&:hover": {
+            backgroundColor: "#C84B31",
+            transform: "scale(1.05)",
+            transition: "all 0.2s ease-in-out"
+        },
+    }));
+
     const handleOnClick = () => {
         handlerFunction();
     }
