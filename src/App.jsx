dcd639203204335267d566c23c594aedd773c335
Saul Castillo <secch97@gmail.com>
2023-08-18 20:40:14 -0400

    1. Users management route created. 2. RegisterUsers page created.

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ import Weeks from './pages/Admin/Weeks/Weeks';
 import MentorHome from './pages/Home/MentorHome';
 import StudentHome from './pages/Home/StudentHome';
 import RegisterOnCohort from './pages/Admin/Users/RegisterOnCohort/RegisterOnCohort';
+import RegisterUsers from './pages/Admin/Users/Register/RegisterUsers';
 /*
     ==========================
     =    AUX MUI VARIABLES   =
@@ -137,6 +138,9 @@ const App = () => {
                   <Route path="" exact element={<Cohorts></Cohorts>}></Route>
                   <Route path=":cohortId" exact element={<Weeks></Weeks>}></Route>
                   <Route path="register/:cohortId" exact element={<RegisterOnCohort></RegisterOnCohort>}></Route>
+                </Route>
+                <Route path="/users" exact>
+                  <Route path="" exact element={<RegisterUsers></RegisterUsers>}></Route>
                 </Route>
               </Route>
             </Route>
---
+++ src/pages/Admin/Users/Register/RegisterUsers.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RegisterUsers = () => {
+    return (
+        <div>RegisterUsers</div>
+    )
+};
+
+export default RegisterUsers;
