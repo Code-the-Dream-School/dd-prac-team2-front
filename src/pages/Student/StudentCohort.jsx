b91a6d6adc2ae472b239fae9d5067041d703d99c
Mbareck21 <missara.lamine@gmail.com>
2023-08-26 13:56:12 -0500

    Added Prettier Json and ignore files && displayed Week start and end dates

--- src/pages/Student/StudentCohort.jsx
+++ src/pages/Student/StudentCohort.jsx
@@ -13,23 +13,25 @@ import {
   CardContent,
   CardActions,
   CircularProgress,
-} from "@mui/material";
-import { CancelOutlined, CheckCircleOutlineRounded } from "@mui/icons-material";
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+  Chip,
+  Stack,
+} from '@mui/material';
+import { CancelOutlined, CheckCircleOutlineRounded } from '@mui/icons-material';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 /*
     ==========================
     =     REACT LIBRARIES    =
     ==========================
 */
-import { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 /*
     ==========================
     =        COMPONENTS      =
     ==========================
 */
-import AppButton from "../../components/Button/AppButton";
-import useAuth from "../../hooks/useAuth";
+import AppButton from '../../components/Button/AppButton';
+import useAuth from '../../hooks/useAuth';
 
 const StudentCohort = () => {
   /*
@@ -45,7 +47,7 @@ const StudentCohort = () => {
       ==========================
       =         STATES         =
       ==========================
-  */   
+  */
   const [currentWeek, setCurrentWeek] = useState();
   const [loading, setLoading] = useState(false);
   const cohortId = state._id;
@@ -57,14 +59,15 @@ const StudentCohort = () => {
       ==========================
   */
   const isUserOnSession = (sessionID) => {
-    const sessionToBeChecked = currentWeek.sessions.filter((session)=>session._id === sessionID);
-    if(sessionToBeChecked[0].participant.includes(auth.userId)){
+    const sessionToBeChecked = currentWeek.sessions.filter(
+      (session) => session._id === sessionID
+    );
+    if (sessionToBeChecked[0].participant.includes(auth.userId)) {
       return true;
-    }
-    else{
+    } else {
       return false;
     }
-  }
+  };
 
   /*
       ==========================
@@ -74,126 +77,101 @@ const StudentCohort = () => {
   const getCurrentWeek = async () => {
     setLoading(true);
     const { data } = await axiosPrivate.get(`/week/${cohortId}/current`);
+    // console.log(data.currentWeek);
     setCurrentWeek(data.currentWeek);
     setLoading(false);
   };
 
   const handleConfirmStatus = async (sessionID) => {
-    if(!isUserOnSession(sessionID)){
+    if (!isUserOnSession(sessionID)) {
       setLoading(true);
-      try{
+      try {
         const { data } = await axiosPrivate.patch(
           `/session/${sessionID}/student/updateStatus`,
           {
             status: true,
           }
         );
-        setCurrentWeek((prevState) => 
-          (
-            {
-              ...prevState,
-              sessions: prevState.sessions.map(
-                (session) => {
-                  if(session._id===sessionID){
-                    return (
-                      {
-                        ...session,
-                        participant: [
-                          ...session.participant,
-                          auth.userId
-                        ]
-                      }
-                    );
-                  }
-                  else{
-                    return session;
-                  }
-                }
-              )
+        setCurrentWeek((prevState) => ({
+          ...prevState,
+          sessions: prevState.sessions.map((session) => {
+            if (session._id === sessionID) {
+              return {
+                ...session,
+                participant: [...session.participant, auth.userId],
+              };
+            } else {
+              return session;
             }
-          )
-        )
+          }),
+        }));
         setLoading(false);
-      }
-      catch(error){
-        if(error.response.status === 403){
+      } catch (error) {
+        if (error.response.status === 403) {
           setLoading(false);
           //User is required to validate auth again
-          navigate("/login", {state:{from: location}, replace: true});
+          navigate('/login', { state: { from: location }, replace: true });
           setAuth({
-              userId: "",
-              userName: "",
-              userEmail: "",
-              role: [],
-              loggedIn: false,
-              avatarUrl: "",
-              isActive: undefined,
-              accessToken: ""
+            userId: '',
+            userName: '',
+            userEmail: '',
+            role: [],
+            loggedIn: false,
+            avatarUrl: '',
+            isActive: undefined,
+            accessToken: '',
           });
-        }
-        else{
+        } else {
           setLoading(false);
           console.error(error);
-        }   
-      }          
+        }
+      }
     }
   };
 
   const handleCancelStatus = async (sessionID) => {
-    if(isUserOnSession(sessionID)){
+    if (isUserOnSession(sessionID)) {
       setLoading(true);
-      try{
-        await axiosPrivate.patch(
-          `/session/${sessionID}/student/updateStatus`,
-          {
-            status: false,
-          }
-        );
-        setCurrentWeek((prevState) => 
-          (
-            {
-              ...prevState,
-              sessions: prevState.sessions.map(
-                (session) => {
-                  if(session._id===sessionID){
-                    return (
-                      {
-                        ...session,
-                        participant: session.participant.filter((participant) => participant!==auth.userId)
-                      }
-                    );
-                  }
-                  else{
-                    return session;
-                  }
-                }
-              )
+      try {
+        await axiosPrivate.patch(`/session/${sessionID}/student/updateStatus`, {
+          status: false,
+        });
+        setCurrentWeek((prevState) => ({
+          ...prevState,
+          sessions: prevState.sessions.map((session) => {
+            if (session._id === sessionID) {
+              return {
+                ...session,
+                participant: session.participant.filter(
+                  (participant) => participant !== auth.userId
+                ),
+              };
+            } else {
+              return session;
             }
-          )
-        );    
+          }),
+        }));
         setLoading(false);
-      }
-      catch(error){
-        if(error.response.status === 403){
+      } catch (error) {
+        if (error.response.status === 403) {
           setLoading(false);
           //User is required to validate auth again
-          navigate("/login", {state:{from: location}, replace: true});
+          navigate('/login', { state: { from: location }, replace: true });
           setAuth({
-              userId: "",
-              userName: "",
-              userEmail: "",
-              role: [],
-              loggedIn: false,
-              avatarUrl: "",
-              isActive: undefined,
-              accessToken: ""
+            userId: '',
+            userName: '',
+            userEmail: '',
+            role: [],
+            loggedIn: false,
+            avatarUrl: '',
+            isActive: undefined,
+            accessToken: '',
           });
+        } else {
+          setLoading(false);
+          console.error(error);
         }
-        else{
-            setLoading(false);
-            console.error(error);
-        }   
-      } 
+      }
     }
   };
 
@@ -213,20 +191,20 @@ const StudentCohort = () => {
     <Container>
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          bgcolor: "transparent",
+          display: 'flex',
+          justifyContent: 'center',
+          bgcolor: 'transparent',
         }}
       >
         <Typography
-          component={"h1"}
+          component={'h1'}
           sx={{
-            backgroundColor: "#C84B31",
+            backgroundColor: '#C84B31',
             borderRadius: 2,
             padding: 2,
             margin: 1,
-            textAlign: "center",
-            fontWeight: "bold",
+            textAlign: 'center',
+            fontWeight: 'bold',
             fontSize: 25,
           }}
         >
@@ -235,20 +213,40 @@ const StudentCohort = () => {
       </Box>
       <Typography
         sx={{
-          backgroundColor: "#C84B31",
+          backgroundColor: '#C84B31',
           borderRadius: 2,
           padding: 1,
           margin: 1,
-          textAlign: "center",
-          fontWeight: "bold",
+          textAlign: 'center',
+          fontWeight: 'bold',
           fontSize: 25,
         }}
       >
         {currentWeek?.name}
+
+        <Stack
+          direction={{ xs: 'column', sm: 'row' }}
+          spacing={{ xs: 1, sm: 2, md: 4 }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Chip
+            variant="outlined"
+            label={`Start Date:  ${new Date(
+              currentWeek?.start
+            ).toLocaleDateString()}`}
+          />
+          <Chip
+            variant="outlined"
+            label={`End Date:  ${new Date(
+              currentWeek?.end
+            ).toLocaleDateString()}`}
+          />
+        </Stack>
       </Typography>
       {loading ? (
         <Box
-          sx={{ display: "flex", justifyContent: "center", paddingBlock: 2 }}
+          sx={{ display: 'flex', justifyContent: 'center', paddingBlock: 2 }}
         >
           <CircularProgress />
         </Box>
@@ -259,10 +257,10 @@ const StudentCohort = () => {
               <Card
                 key={session._id}
                 sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  marginTop: "1px",
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  marginTop: '1px',
                 }}
               >
                 <Box>
@@ -287,67 +285,61 @@ const StudentCohort = () => {
                   </CardContent>
                 </Box>
                 <CardActions>
-                  {
-                    session.participant.includes(auth.userId) ?
-                    (
-                      <>
-                        <AppButton
-                          text={"Yes"}
-                          type="button"
-                          width="auto"
-                          color={"#609966"}
-                          handlerFunction={() => handleConfirmStatus(session._id)}
-                        >
-                          <CheckCircleOutlineRounded></CheckCircleOutlineRounded>
-                        </AppButton>
-                        <AppButton
-                          text={"No"}
-                          type="button"
-                          width="auto"
-                          color={"white"}
-                          textColor={"#1A1A2E"}
-                          handlerFunction={() => handleCancelStatus(session._id)}
-                        >
-                          <CancelOutlined></CancelOutlined>
-                        </AppButton>
-                      </>
-                    )
-                    :
-                    (
-                      <>
-                        <AppButton
-                          text={"Yes"}
-                          type="button"
-                          width="auto"
-                          color={"white"}
-                          textColor={"#1A1A2E"}
-                          handlerFunction={() => handleConfirmStatus(session._id)}
-                        >
-                          <CheckCircleOutlineRounded></CheckCircleOutlineRounded>
-                        </AppButton>
-                        <AppButton
-                          text={"No"}
-                          type="button"
-                          width="auto"
-                          color="#CD1818"
-                          handlerFunction={() => handleCancelStatus(session._id)}
-                        >
-                          <CancelOutlined></CancelOutlined>
-                        </AppButton>
-                      </>
-                    )
-                  }
-                  
+                  {session.participant.includes(auth.userId) ? (
+                    <>
+                      <AppButton
+                        text={'Yes'}
+                        type="button"
+                        width="auto"
+                        color={'#609966'}
+                        handlerFunction={() => handleConfirmStatus(session._id)}
+                      >
+                        <CheckCircleOutlineRounded></CheckCircleOutlineRounded>
+                      </AppButton>
+                      <AppButton
+                        text={'No'}
+                        type="button"
+                        width="auto"
+                        color={'white'}
+                        textColor={'#1A1A2E'}
+                        handlerFunction={() => handleCancelStatus(session._id)}
+                      >
+                        <CancelOutlined></CancelOutlined>
+                      </AppButton>
+                    </>
+                  ) : (
+                    <>
+                      <AppButton
+                        text={'Yes'}
+                        type="button"
+                        width="auto"
+                        color={'white'}
+                        textColor={'#1A1A2E'}
+                        handlerFunction={() => handleConfirmStatus(session._id)}
+                      >
+                        <CheckCircleOutlineRounded></CheckCircleOutlineRounded>
+                      </AppButton>
+                      <AppButton
+                        text={'No'}
+                        type="button"
+                        width="auto"
+                        color="#CD1818"
+                        handlerFunction={() => handleCancelStatus(session._id)}
+                      >
+                        <CancelOutlined></CancelOutlined>
+                      </AppButton>
+                    </>
+                  )}
                 </CardActions>
               </Card>
             ))
           ) : (
             <Typography
               sx={{
-                backgroundColor: "#f2f2f2",
+                backgroundColor: '#f2f2f2',
                 padding: 2,
                 borderRadius: 2,
-                textAlign: "center",
+                textAlign: 'center',
               }}
             >
               No sessions scheduled for this week
