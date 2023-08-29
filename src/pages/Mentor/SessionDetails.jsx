8fb90899fe1e762300a81e45253a08c94bb9567b
cdpaiva <cdpaivaa@gmail.com>
2023-08-28 23:12:50 -0400

    Finishes first version of mentor session details

--- src/pages/Mentor/SessionDetails.jsx
+++ src/pages/Mentor/SessionDetails.jsx
@@ -3,37 +3,23 @@ import {
   Container,
   Typography,
   List,
-  Card,
   CardContent,
-  Chip,
   ListItem,
-  ListItemText,
+  TextField,
+  Button,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
-import { useNavigate } from "react-router-dom";
-import AuthFormControl from "../../components/FormControl/AuthFormControl";
-import FormTextField from "../../components/TextField/FormTextField";
-import AppButton from "../../components/Button/AppButton";
 import useAuth from "../../hooks/useAuth";
 
 const MentorSession = () => {
+  const { sessionId } = useParams();
+  const axiosPrivate = useAxiosPrivate();
+  const { auth } = useAuth();
   const [currentSession, setCurrentSession] = useState();
   const [loading, setLoading] = useState(true);
-  const axiosPrivate = useAxiosPrivate();
-  const navigate = useNavigate();
-  const { sessionId } = useParams();
-  const [reset, setReset] = useState(false);
-  //   const [userStatus, setUserStatus] = useState();
-  const { auth, setAuth } = useAuth();
   const [comment, setComment] = useState();
-  const [formError, setFormError] = useState({
-    commentError: {
-      error: false,
-      errorMessage: "Please enter a valid comment",
-    },
-  });
 
   const getCurrentSession = async () => {
     setLoading(true);
@@ -41,22 +27,6 @@ const MentorSession = () => {
     setCurrentSession(data.session);
     setLoading(false);
   };
-
-  console.log(currentSession);
-
-  //   const loggedInUserStatus = async () => {
-  //     setLoading(true);
-  //     const { data } = await axiosPrivate.get(
-  //       `/session/${sessionId}/student/status`
-  //     );
-  //     console.log(data);
-  //     if (data) {
-  //       setUserStatus("Confirm");
-  //     } else {
-  //       setUserStatus("Not Joining");
-  //     }
-  //     setLoading(false);
-  //   };
 
   const handleCommentSubmit = async (e) => {
     e.preventDefault();
@@ -66,37 +36,20 @@ const MentorSession = () => {
       content: e.target.comment.value.trim(),
       sessionId: sessionId,
     });
-    console.log(data);
+
+    const newComment = data.comment;
+    newComment.name = { _id: auth.userId, name: auth.userName };
+    const newSession = {
+      ...currentSession,
+      discussion: [...currentSession.discussion, newComment],
+    };
+    setCurrentSession(newSession);
     setComment("");
   };
 
-  // const getComment = async () => {
-  //   setLoading(true);
-  //   const { data } = await axiosPrivate.get("/session/comment");
-  //   console.log(data);
-  //   // setComment((prev)=>[...prev, data]);
-  //   setLoading(false);
-  // };
-
   useEffect(() => {
     getCurrentSession();
-    // loggedInUserStatus();
-    // getComment();
   }, [sessionId]);
-
-  const userStatus =
-    currentSession?.creator._id === auth.userId ||
-    currentSession?.participant?.includes(auth.userId);
-
-  const handleCommentError = (inputError) => {
-    setFormError((prevState) => ({
-      ...prevState,
-      commentError: {
-        ...prevState.commentError,
-        error: inputError,
-      },
-    }));
-  };
 
   const convertDate = (date) => {
     if (!date) {
@@ -123,7 +76,7 @@ const MentorSession = () => {
     return initials;
   };
 
-  const getRandomColor = () => {
+  const getColor = (name) => {
     const colors = [
       "#FFD4B2",
       "#FFF6BD",
@@ -132,8 +85,8 @@ const MentorSession = () => {
       "#FD8A8A",
       "#9EA1D4",
     ];
-    const i = Math.floor(Math.random() * colors.length);
-    return colors[i];
+    const letterNum = name[0].toLowerCase().charCodeAt(0) - 97;
+    return colors[letterNum % colors.length];
   };
 
   return (
@@ -141,7 +94,7 @@ const MentorSession = () => {
       {loading ? (
         <h1>Loading</h1>
       ) : (
-        <Container sx={{ backgroundColor: "#f2f2f2" }}>
+        <Container sx={{ backgroundColor: "#f2f2f2", marginBlock: 4 }}>
           <Typography component="h1" sx={{ fontSize: "2rem" }}>
             Session Details
           </Typography>
@@ -184,26 +137,6 @@ const MentorSession = () => {
             </Box>
           </Box>
 
-          <Box
-            sx={{
-              display: "flex",
-              gap: 1,
-              alignItems: "center",
-              marginBlock: 2,
-            }}
-          >
-            <Typography
-              component="h3"
-              sx={{ fontSize: "1.5rem", paddingBlock: 1 }}
-            >
-              Current status:
-            </Typography>
-            {userStatus ? (
-              <Chip label="Confirmed" color="success"></Chip>
-            ) : (
-              <Chip label="Not Confirmed" color="warning"></Chip>
-            )}
-          </Box>
           <Box>
             <Typography
               component="h3"
@@ -222,7 +155,7 @@ const MentorSession = () => {
                         height: "40px",
                         lineHeight: "40px",
                         textAlign: "center",
-                        backgroundColor: getRandomColor(),
+                        backgroundColor: getColor(student.name),
                       }}
                     >
                       {getInitials(student.name)}
@@ -237,18 +170,37 @@ const MentorSession = () => {
               <Typography>No students in this session at the moment</Typography>
             )}
           </Box>
-          <Box>
-            <Typography
-              component="h3"
-              sx={{ fontSize: "1.5rem", paddingBlock: 1 }}
-            >
-              Discussion:
-            </Typography>
+          <Typography
+            component="h3"
+            sx={{ fontSize: "1.5rem", paddingBlock: 1 }}
+          >
+            Discussion:
+          </Typography>
+
+          <Box
+            component="form"
+            autoComplete="off"
+            onSubmit={handleCommentSubmit}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <TextField
+              required
+              type="text"
+              label="Add new comment"
+              name="comment"
+              value={comment}
+            ></TextField>
+            <Button type="submit">Add new comment</Button>
           </Box>
-          <List>
+
+          <List sx={{ backgroundColor: "#fefefe", marginBlockEnd: 4 }}>
             {currentSession?.discussion?.length > 0 ? (
               currentSession?.discussion.map((d) => (
-                <Card
+                <Box
                   key={d._id}
                   sx={{
                     display: "flex",
@@ -266,7 +218,7 @@ const MentorSession = () => {
                       <Typography>{d.name.name}</Typography>
                     </CardContent>
                   </Box>
-                </Card>
+                </Box>
               ))
             ) : (
               <Typography>No discussions in this session</Typography>
@@ -276,220 +228,6 @@ const MentorSession = () => {
       )}
     </>
   );
-  // return (
-  //   <Container>
-  //     <Box
-  //       sx={{
-  //         display: "flex",
-  //         justifyContent: "center",
-  //         bgcolor: "transparent",
-  //       }}
-  //     >
-  //       <Typography
-  //         component={"h1"}
-  //         sx={{
-  //           backgroundColor: "#C84B31",
-  //           borderRadius: 2,
-  //           padding: 2,
-  //           margin: 1,
-  //           textAlign: "center",
-  //           fontWeight: "bold",
-  //           fontSize: 25,
-  //         }}
-  //       >
-  //         {currentSession?.type} Session
-  //       </Typography>
-  //       <Typography
-  //         sx={{
-  //           backgroundColor: "#C84B31",
-  //           borderRadius: 2,
-  //           padding: 2,
-  //           margin: 1,
-  //           textAlign: "center",
-  //           fontWeight: "bold",
-  //           fontSize: 25,
-  //         }}
-  //       >
-  //         {currentSession?.creator.name}
-  //       </Typography>
-  //       <Typography
-  //         sx={{
-  //           backgroundColor: "#055c1c",
-  //           borderRadius: 2,
-  //           padding: 2,
-  //           margin: 1,
-  //           textAlign: "center",
-  //           fontWeight: "bold",
-  //           fontSize: 25,
-  //         }}
-  //       >
-  //         My Current Status: {userStatus ? "Confirmed" : "Not confirmed"}
-  //       </Typography>
-  //     </Box>
-  //     <Typography
-  //       sx={{
-  //         backgroundColor: "#ffffff",
-  //         borderRadius: 2,
-  //         padding: 1,
-  //         margin: 1,
-  //         textAlign: "center",
-  //         fontSize: 20,
-  //       }}
-  //     >
-  //       Start: {currentSession?.start}
-  //     </Typography>
-  //     <Typography
-  //       sx={{
-  //         backgroundColor: "#ffffff",
-  //         borderRadius: 2,
-  //         padding: 1,
-  //         margin: 1,
-  //         textAlign: "center",
-  //         fontSize: 20,
-  //       }}
-  //     >
-  //       End: {currentSession?.end}
-  //     </Typography>
-  //     <Typography
-  //       sx={{
-  //         backgroundColor: "#f2f2f2",
-  //         padding: 2,
-  //         borderRadius: 2,
-  //       }}
-  //     >
-  //       Confirmed participation:
-  //     </Typography>
-  //     <List>
-  //       {currentSession?.participant?.length > 0 ? (
-  //         currentSession?.participant.map((p) => (
-  //           <Card
-  //             key={p._id}
-  //             sx={{
-  //               display: "flex",
-  //               justifyContent: "space-between",
-  //               alignItems: "center",
-  //             }}
-  //           >
-  //             <Box>
-  //               <CardContent>
-  //                 <Typography variant="h5">{`${p.name}`}</Typography>
-  //               </CardContent>
-  //             </Box>
-  //           </Card>
-  //         ))
-  //       ) : (
-  //         <Typography
-  //           sx={{
-  //             backgroundColor: "#f2f2f2",
-  //             padding: 2,
-  //             borderRadius: 2,
-  //             textAlign: "center",
-  //           }}
-  //         >
-  //           No students in this session
-  //         </Typography>
-  //       )}
-  //     </List>
-  //     <br />
-  //     <br />
-
-  //     <Typography
-  //       component={"h1"}
-  //       sx={{
-  //         backgroundColor: "#C84B31",
-  //         borderRadius: 2,
-  //         padding: 2,
-  //         margin: 1,
-  //         textAlign: "center",
-  //         fontWeight: "bold",
-  //         fontSize: 25,
-  //       }}
-  //     >
-  //       Discussion
-  //     </Typography>
-
-  //     <Box
-  //       component={"form"}
-  //       sx={{
-  //         display: "flex",
-  //         flexDirection: "column",
-  //         justifyContent: "center",
-  //         alignItems: "center",
-  //         width: "100%",
-  //       }}
-  //       autoComplete="off"
-  //       onSubmit={handleCommentSubmit}
-  //     >
-  //       <div>
-  //         <AuthFormControl width="75%">
-  //           <Box
-  //             sx={{
-  //               display: "flex",
-  //               flexDirection: "column",
-  //               justifyContent: "center",
-  //             }}
-  //           >
-  //             <br></br>
-  //           </Box>
-  //           <FormTextField
-  //             required
-  //             type="text"
-  //             label="Comment:"
-  //             name="comment"
-  //             isFocused={true}
-  //             width="100%"
-  //             variant="light"
-  //             reset={reset}
-  //             value={comment}
-  //             onHandleError={handleCommentError}
-  //           ></FormTextField>
-  //         </AuthFormControl>
-  //         <AppButton
-  //           text={"Add new comment"}
-  //           type="submit"
-  //           width="25%"
-  //           handlerFunction={() => {}}
-  //         />
-  //       </div>
-  //     </Box>
-  //     <List>
-  //       {currentSession?.discussion?.length > 0 ? (
-  //         currentSession?.discussion.map((d) => (
-  //           <Card
-  //             key={d._id}
-  //             sx={{
-  //               display: "flex",
-  //               justifyContent: "space-between",
-  //               alignItems: "center",
-  //             }}
-  //           >
-  //             <Box>
-  //               <CardContent>
-  //                 <Typography variant="h5">{`${d.content}`}</Typography>
-  //               </CardContent>
-  //             </Box>
-  //             <Box>
-  //               <CardContent>
-  //                 <Typography>{`${d.name.name}`}</Typography>
-  //               </CardContent>
-  //             </Box>
-  //           </Card>
-  //         ))
-  //       ) : (
-  //         <Typography
-  //           sx={{
-  //             backgroundColor: "#f2f2f2",
-  //             padding: 2,
-  //             borderRadius: 2,
-  //             textAlign: "center",
-  //           }}
-  //         >
-  //           No discussions in this session
-  //         </Typography>
-  //       )}
-  //     </List>
-  //   </Container>
-  // );
 };
 
 export default MentorSession;
