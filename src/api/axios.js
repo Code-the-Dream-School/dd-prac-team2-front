c3a7c33451792a6b7d1dbb8833a3597e2fe20328
Saul Castillo <secch97@gmail.com>
2023-09-06 23:25:12 -0400

    Added development endpoings

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://prac-team2.onrender.com/";
+const BASE_URL = "http://localhost:8000/api/v1";
 
 export default axios.create({
   baseURL: BASE_URL,
