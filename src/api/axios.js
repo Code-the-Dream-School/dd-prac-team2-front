ca946f2f707849b45e67931925e5a3bfb2ce5ce5
Saul Castillo <secch97@gmail.com>
2023-09-07 23:33:50 -0400

    Set up merge with main branch

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://prac-team2.onrender.com/api/v1";
 
 export default axios.create({
   baseURL: BASE_URL,
