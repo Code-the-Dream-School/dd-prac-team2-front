30ae5add97edf802e844d26fa6f71dfe9496ad42
Saul Castillo <secch97@gmail.com>
2023-09-10 14:41:06 -0400

    1. Quick fix

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -24,23 +24,29 @@ const useRefreshToken = () => {
         ==========================
     */
   const refresh = async () => {
-    const response = await axios.get(`auth/refreshToken`, {
-      withCredentials: true,
-    });
-    setAuth((prevAuth) => {
-      return {
-        userId: response.data.user.id,
-        slackId: response.data.user.slackId,
-        userName: response.data.user.name,
-        userEmail: response.data.user.email,
-        role: response.data.user.roles,
-        loggedIn: true,
-        avatarUrl: response.data.user.avatarUrl,
-        isActive: response.data.user.isActivated,
-        accessToken: response.data.token,
-      };
-    });
-    return response.data.token;
+    try{
+      const response = await axios.get(`auth/refreshToken`, {
+        withCredentials: true,
+      });
+      console.log(refresh);
+      setAuth((prevAuth) => {
+        return {
+          userId: response.data.user.id,
+          slackId: response.data.user.slackId,
+          userName: response.data.user.name,
+          userEmail: response.data.user.email,
+          role: response.data.user.roles,
+          loggedIn: true,
+          avatarUrl: response.data.user.avatarUrl,
+          isActive: response.data.user.isActivated,
+          accessToken: response.data.token,
+        };
+      });
+      return response.data.token;
+    }
+    catch(error){
+      console.error(error);
+    }
   };
   return refresh;
 };
